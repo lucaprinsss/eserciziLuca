701326e072752611fc19f09ee34bc48e770407c7
Luca <s259177@studenti.polito.it>
2024-06-10 18:38:49 +0200

    Terminato ma non funzionano alcuni metodi

--- week9/react-qa/src/components/AnswerForm.jsx
+++ week9/react-qa/src/components/AnswerForm.jsx
@@ -1,17 +1,29 @@
+import dayjs from 'dayjs';
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-function AnswerForm() {
-  const [text, setText] = useState('');
-  const [email, setEmail] = useState('');
-  const [date, setDate] = useState('');
+function AnswerForm(props) {
+  const [text, setText] = useState(props.answer? props.answer.text : '');
+  const [email, setEmail] = useState(props.answer? props.answer.email : '');
+  const [date, setDate] = useState(props.answer? props.answer.date.format('YYYY-MM-DD') : dayjs().format('YYYY-MM-DD'));
 
-  const handleSubmit = () => {
-    // farà qualcosa
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //Creo una nuova risposta (un oggetto che contiene i valori, la risposta viene creata nel metodo addAnswer() in App.jsx)
+    const answer = {text, email, date};
+    //TODO: aggiungere validazioni
+    
+    if(props.mode==='edit') {
+      //aggiorno la risposta
+      props.updateAnswer({id: props.answer.id , ...[answer]});
+    } else {
+      //aggiungo la risposta (allo stato answers di App.jsx)
+      props.addAnswer(answer);
+    }
   }
 
   return(
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className='mb-3'>
         <Form.Label>Text</Form.Label>
         <Form.Control type="text" required={true} minLength={2} value={text} onChange={(event) => setText(event.target.value)}></Form.Control>
@@ -24,7 +36,9 @@ function AnswerForm() {
         <Form.Label>Date</Form.Label>
         <Form.Control type="date" value={date} onChange={(event) => setDate(event.target.value)}></Form.Control>
       </Form.Group>
-      <Button variant='primary' type='Submit'>Add</Button> <Button variant='danger'>Cancel</Button>
+      {props.mode==='add'&&<Button variant='success' type='Submit'>Add</Button>}
+      {props.mode==='edit'&&<Button variant='success' type='Submit'>Update</Button>}
+      <Button variant='danger' onClick={props.cancel}>Cancel</Button>
     </Form>
   );
 }
